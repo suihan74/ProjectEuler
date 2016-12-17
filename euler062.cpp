f69209c7c3aea06687504ba7a3b45860d5dc71fe
suihan <suihan742@gmail.com>
2016-12-18 01:22:41 +0900

    [Modify]  make e062 simple

--- euler062.cpp
+++ euler062.cpp
@@ -6,59 +6,30 @@
  * 立方数になるような桁の置換をちょうど5つもつ最小の立方数を求めよ.
  */
 
+#include <algorithm>
 #include <array>
 #include <cstdint>
 #include <iostream>
+#include <string>
 
-using uInt = std::uint_fast64_t;           // 32bitだと溢れる
-using DigitsArray = std::array<uInt, 10>;  // 0~9だけ数えればいいので配列サイズはたかだか10個
-
-/**
- * 正の整数numの各桁に現れる数字それぞれの個数を連想配列に記録して返す
- * @param  num 対象の数値
- * @return     numの各桁に含まれる数字がそれぞれ何個ずつ含まれているかを記した配列
- */
-DigitsArray count_digits_num(uInt num)
-{
-  DigitsArray arr;
-  arr.fill(0);
-  do {
-    const uInt d = num % 10;
-    num /= 10;
-    arr[d]++;
-  } while (num);
-  return arr;
-}
-
-/**
- * 二つの数値に対応する使用数字配列が一致するかを確認する
- * @param arr_a  ある数num_aの桁に含まれる数字の個数を記録した配列
- * @param arr_b  ある数num_bの桁に含まれる数字の個数を記録した配列
- */
-inline
-bool equals_digits_num(const DigitsArray& arr_a, const DigitsArray& arr_b)
-{
-  constexpr auto SIZE = arr_a.size();
-  for (uInt i = 0; i < SIZE; i++) {
-    if (arr_a[i] != arr_b[i]) { return false; }
-  }
-  return true;
-}
+using uInt = std::uint_fast64_t; // 32bitだと溢れる
 
 int main(void)
 {
-  // 予め上限を決めて，立方数とその各桁に含まれる数字の個数記録のペアでできた配列を作っておく
+  // 予め上限を決めて，立方数とその各桁に含まれる数字と文字列化・ソートしたもののペアでできた配列を作っておく
   constexpr uInt MAX = 10000;
-  std::array<std::pair<uInt, DigitsArray>, MAX> arr;
+  std::array<std::pair<uInt, std::string>, MAX> arr;
   for (uInt i = 0; i < MAX; i++) {
     const uInt i3 = i * i * i;
-    arr.at(i) = std::make_pair(i3, count_digits_num(i3));
+    auto str = std::to_string(i3);
+    std::sort(str.begin(), str.end());
+    arr.at(i) = std::make_pair(i3, std::move(str));
   }
 
   for (uInt i = 0; i < MAX; i++) {
     uInt count = 1;   // 0ではなく1なのは，予め自分自身をカウントしているため
     for (uInt j = i + 1; j < MAX; j++) {
-      if (equals_digits_num(arr[i].second, arr[j].second)) {
+      if (arr[i].second == arr[j].second) {  // ソート後文字列が一致すれば置換で表せる
         count++;
       }
     }
