a8ae78a20a37fcc69ec5282440be7e2506a9e2f5
suihan <suihan742@gmail.com>
2017-01-09 01:49:10 +0900

    [Modify] revised e031
    re-solved e031 with dynamic programming

--- euler031.cpp
+++ euler031.cpp
@@ -11,44 +11,22 @@
  * これらの硬貨を使って£2を作る方法は何通りあるか?
  */
 
+#include <array>
 #include <cstdint>
 #include <iostream>
 
 using uInt = std::uint_fast32_t;
 
-/**
- * units[]の中身で合計値leftを作る組み合わせの個数を計算する
- * @param  units    組み合わせに使える硬貨の集合
- * @param  arr_size unitsの配列長
- * @param  left     （この呼び出し階層以下での残りの）合計値
- * @param  idx      unitsの注目インデックス
- * @param  count    今まで見つけた正解の組み合わせ個数
- * @return          この呼び出し階層までで見つけた正解の組み合わせ個数
- */
-uInt get_patterns(
-  const uInt units[],
-  const uInt arr_size,
-  uInt left,
-  uInt idx = 0,
-  uInt count = 0)
-{
-  // 対象の硬貨を使ってleft以下になるパターン数N
-  // 例: left=200p, units[idx]=2p なら2p硬貨は0~100枚まで使える
-  const uInt N = left / units[idx];
-  if (units[idx] * N == left) { count++; }  // leftがunits[idx]で割り切れる => 正解の組み合わせがある
-  for (uInt i = 0; i <= N; i++) {
-    // 対象の硬貨 * 何枚使うか でこの階層の金額を計算
-    const uInt cur = units[idx] * i;
-    if (left > cur && idx + 1 < arr_size) {
-      count = get_patterns(units, arr_size, left - cur, idx + 1, count);
-    }
-  }
-  return count;
-}
-
 int main(void)
 {
-  uInt units[] = {200, 100, 50, 20, 10, 5, 2, 1};
-  std::cout << "Euler031: " << get_patterns(units, 8, 200) << std::endl;
+  constexpr uInt TARGET = 200;
+  std::array<uInt, 8> coins = {1, 2, 5, 10, 20, 50, 100, 200};
+  std::array<uInt, TARGET + 1> memo = {1};
+  for (const uInt coin : coins) {
+    for (uInt i = coin; i <= TARGET; i++) {
+      memo.at(i) += memo.at(i - coin);
+    }
+  }
+  std::cout << "Euler031: " << memo.at(TARGET) << std::endl;
   return 0;
 }
